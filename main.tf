ae756edc1db2a1a208ed46697bfc9239fad11b50
pratheesh-dev-tech <iq0pooki@gmail.com>
2025-04-11 00:59:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,11 +42,9 @@ resource "aws_s3_bucket_policy" "public_read" {
       {
         Sid       = "PublicReadGetObject",
         Effect    = "Allow",
-        Principal = {
-          AWS = ""
-        },
+        Principal = "*",                            # ✅ Corrected here
         Action    = "s3:GetObject",
-        Resource  = "${aws_s3_bucket.static_site.arn}/"
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
       }
     ]
   })
