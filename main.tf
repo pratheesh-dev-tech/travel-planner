c81dd03cd6daf191a7b2098b1f6460ca89cad79d
pratheesh-dev-tech <iq0pooki@gmail.com>
2025-04-11 00:45:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,24 +3,33 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "static_site" {
-bucket = "my-jenkins-terraform-bucket-pk"
-
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
-  }
+  bucket = var.bucket_name
 
   tags = {
-    Name = "StaticWebsite"
+    Name        = "MyStaticSiteBucket"
+    Environment = "Dev"
+  }
+
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_website_configuration" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "block" {
-  bucket = aws_s3_bucket.static_site.id
-
-  block_public_acls   = false
-  block_public_policy = false
-  ignore_public_acls  = false
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.static_site.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
@@ -28,12 +37,13 @@ resource "aws_s3_bucket_policy" "public_read" {
   bucket = aws_s3_bucket.static_site.id
 
   policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
       {
-        Effect    = "Allow",
-        Principal = "",
-        Action    = "s3:GetObject",
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = ""
+        Action    = "s3:GetObject"
         Resource  = "${aws_s3_bucket.static_site.arn}/"
       }
     ]
@@ -41,9 +51,8 @@ resource "aws_s3_bucket_policy" "public_read" {
 }
 
 resource "aws_s3_object" "index" {
-  bucket       = aws_s3_bucket.static_site.bucket
+  bucket       = aws_s3_bucket.static_site.id
   key          = "index.html"
   source       = "${path.module}/index.html"
-  acl          = "public-read"
   content_type = "text/html"
 }
