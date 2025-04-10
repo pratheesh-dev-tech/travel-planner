7183cfd3017b01c1a67c750ac55f28647eb46b16
pratheesh-dev-tech <iq0pooki@gmail.com>
2025-04-11 00:52:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,13 +37,15 @@ resource "aws_s3_bucket_policy" "public_read" {
   bucket = aws_s3_bucket.static_site.id
 
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = ""
-        Action    = "s3:GetObject"
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = {
+          AWS = ""
+        },
+        Action    = "s3:GetObject",
         Resource  = "${aws_s3_bucket.static_site.arn}/"
       }
     ]
